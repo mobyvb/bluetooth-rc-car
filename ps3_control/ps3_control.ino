a6ac46f78d47601205ca43d843e4ee5a8be9f21a
mobyvb <mobyvb@gmail.com>
2013-11-02 19:11:41 -0400

    get values for default controls

--- ps3_control/ps3_control.ino
+++ ps3_control/ps3_control.ino
@@ -1,3 +1,4 @@
+#include <Servo.h> 
 #include <PS3BT.h>
 #include <usbhub.h>
 
@@ -6,10 +7,23 @@ USBHub Hub1(&Usb);
 BTD Btd(&Usb);
 PS3BT PS3(&Btd,0x00,0x15,0x83,0x3D,0x0A,0x57); // This will also store the bluetooth address - this can be obtained from the dongle when running the sketch
 
-boolean printAngle;
+boolean defaultControls = true;
+
+boolean forward = true;
+int motorA = 2;
+int motorB = 4;
+int motorSpeed;
+
+Servo steering;
+int rightBound = 50;
+int leftBound = 110;
+int turnAngle = (rightBound+leftBound)/2;
 
 void setup() {
   Serial.begin(115200);
+  
+  steering.attach(3);
+  
   while (!Serial);
   if (Usb.Init() == -1) {
     Serial.print(F("\r\nOSC did not start"));
@@ -19,29 +33,36 @@ void setup() {
 }
 void loop() {
   Usb.Task();
-
+  
   if(PS3.PS3Connected || PS3.PS3NavigationConnected) {
-    if(PS3.getAnalogHat(LeftHatX) > 137 || PS3.getAnalogHat(LeftHatX) < 117 || PS3.getAnalogHat(LeftHatY) > 137 || PS3.getAnalogHat(LeftHatY) < 117 || PS3.getAnalogHat(RightHatX) > 137 || PS3.getAnalogHat(RightHatX) < 117 || PS3.getAnalogHat(RightHatY) > 137 || PS3.getAnalogHat(RightHatY) < 117) {
-      Serial.print(F("\r\nLeftHatX: ")); 
-      Serial.print(PS3.getAnalogHat(LeftHatX));
-      Serial.print(F("\tLeftHatY: ")); 
-      Serial.print(PS3.getAnalogHat(LeftHatY));
-      if(!PS3.PS3NavigationConnected) {
-        Serial.print(F("\tRightHatX: ")); 
-        Serial.print(PS3.getAnalogHat(RightHatX));
-        Serial.print(F("\tRightHatY: ")); 
-        Serial.print(PS3.getAnalogHat(RightHatY));
-      }      
-    }
-
-    if(PS3.getAnalogButton(L2) || PS3.getAnalogButton(R2)) {
-      Serial.print(F("\r\nL2: ")); 
-      Serial.print(PS3.getAnalogButton(L2));
-      if(!PS3.PS3NavigationConnected) {
-        Serial.print(F("\tR2: ")); 
-        Serial.print(PS3.getAnalogButton(R2));
+    if(defaultControls) {
+      if(PS3.getAnalogHat(LeftHatX) > 137 || PS3.getAnalogHat(LeftHatX) < 117 || PS3.getAnalogHat(LeftHatY) > 137 || PS3.getAnalogHat(LeftHatY) < 117) {
+        turnAngle = map(PS3.getAnalogHat(LeftHatX), 0, 255, leftBound, rightBound);   
+      }
+      else {
+        turnAngle = (rightBound+leftBound)/2;
+      }
+  
+      if(PS3.getAnalogButton(L2)) {
+        motorSpeed = PS3.getAnalogButton(L2);
+        forward = false;
+      }
+      else if(PS3.getAnalogButton(R2)) {
+        motorSpeed = PS3.getAnalogButton(R2);
+        forward = true;
+      }
+      else {
+        motorSpeed = 0;
       }
     }
+    
+    else {
+      Serial.print(F("\r\nPitch: "));               
+      Serial.print(PS3.getAngle(Pitch));                  
+      Serial.print(F("\tRoll: ")); 
+      Serial.print(PS3.getAngle(Roll));
+    }
+    
     if(PS3.getButtonClick(PS)) {
       Serial.print(F("\r\nPS"));
       PS3.disconnect();
@@ -49,14 +70,9 @@ void loop() {
     else {
       if(PS3.getButtonClick(START)) {
         Serial.print(F("\r\nStart"));              
-        printAngle = !printAngle;
+        defaultControls = !defaultControls;
       }                           
     }
-    if(printAngle) {
-      Serial.print(F("\r\nPitch: "));               
-      Serial.print(PS3.getAngle(Pitch));                  
-      Serial.print(F("\tRoll: ")); 
-      Serial.print(PS3.getAngle(Roll));
-    }
   }
+  delay(10);
 }
